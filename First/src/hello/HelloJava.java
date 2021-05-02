affda479234bba6847bc7a313dcc508a463ca997
CBJ <CBJ@DESKTOP-8KFK27D>
2021-05-03 02:46:26 +0900

    여러줄 주석

--- First/src/hello/HelloJava.java
+++ First/src/hello/HelloJava.java
@@ -5,6 +5,9 @@ public class HelloJava {
 	public static void main(String[] args) {
 		System.out.println("Hello, Java");
 		//주석 연습
-		//2021년 5월 3일 2시45분
+		/*2021년
+		 * 5월3일 
+		 * 2시45분
+		 */
 	}
 }
