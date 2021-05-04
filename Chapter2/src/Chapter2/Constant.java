6b9ef6d69d32518dbfc9b4ce4771e64a0ea52f7a
choibyoungjin <choibyoungjin85@gmail.com>
2021-05-04 16:30:49 +0900

    상수 사용하기

---
+++ Chapter2/src/Chapter2/Constant.java
@@ -0,0 +1,18 @@
+package Chapter2;
+
+public class Constant {
+
+	public static void main(String[] args) {
+		final int MAX_NUM = 100;//선언 동시에 초기화//final 예약어로 상수 선언
+		final int MIN_NUM;
+		
+		MIN_NUM = 0;//사용 전 초기화 필요
+		
+		System.out.println(MAX_NUM);
+		System.out.println(MIN_NUM);
+		
+		//MAX_NUM = 1000; //상수는 값 변경 불가
+
+	}
+
+}
