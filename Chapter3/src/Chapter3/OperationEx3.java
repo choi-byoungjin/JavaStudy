b053a19db9c08857c43971a7b64a064e497487f2
choibyoungjin <choibyoungjin85@gmail.com>
2021-05-07 03:34:43 +0900

    단락 회로 주석

--- Chapter3/src/Chapter3/OperationEx3.java
+++ Chapter3/src/Chapter3/OperationEx3.java
@@ -6,15 +6,15 @@ public class OperationEx3 {
 		int num1 = 10;
 		int i = 2;
 		
-		boolean value = ((num1 = num1 + 10) < 10) && ((i = i + 2) < 10);
+		boolean value = ((num1 = num1 + 10) < 10) && ((i = i + 2) < 10);//논리 곱에서 앞 항의 결과 값이 거짓이므로 뒷 항은 실행되지 않는다.
 		System.out.println(value);
 		System.out.println(num1);
 		System.out.println(i);
 		
-		value = ((num1 = num1 + 10) > 10) || ((i = i + 2) < 10);
+		value = ((num1 = num1 + 10) > 10) || ((i = i + 2) < 10);//논리 합에서 앞 항의 결과 값이 참이므로 뒷 항은 실행되자 않는다.
 		System.out.println(value);
 		System.out.println(num1);
 		System.out.println(i);
 	}
-
+//나머지 항이 실행되지 않는 것을 '단락 회로 평가'(Short Circuit Evaluation; SCE)라 한다.
 }
