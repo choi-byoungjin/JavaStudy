0dbcc98428eb82ec5a81d4ccb5f38cf9bfae6787
choibyoungjin <choibyoungjin85@gmail.com>
2021-05-08 00:42:31 +0900

    switch-case 나 혼자 코딩

---
+++ Chapter4/src/Chapter4/switch_case_Coding.java
@@ -0,0 +1,28 @@
+package Chapter4;
+
+public class switch_case_Coding {
+
+	public static void main(String[] args) {
+		int floor = 4;
+		
+		switch(floor) {
+		case 1 :
+			System.out.println(floor + "층 약국입니다.");
+			break;
+		case 2 :
+			System.out.println(floor + "층 정형외과입니다.");
+			break;
+		case 3 :
+			System.out.println(floor + "층 피부과입니다.");
+			break;
+		case 4 :
+			System.out.println(floor + "층 치과입니다.");
+			break;
+		case 5 :
+			System.out.println(floor + "층 헬스클럽입니다.");
+			break;
+		}
+
+	}
+
+}
