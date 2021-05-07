f50c05f2da101a9e3c08ad534758cbe7bf4f488e
choibyoungjin <choibyoungjin85@gmail.com>
2021-05-08 01:50:50 +0900

    for문 나 혼자 코딩

---
+++ Chapter4/src/Chapter4/ForCoding.java
@@ -0,0 +1,13 @@
+package Chapter4;
+
+public class ForCoding {
+
+	public static void main(String[] args) {
+		int i;
+		for(i = 0; i <= 10; i++) {
+			System.out.println("¾È³çÇÏ¼¼¿ä" + i);
+		}
+
+	}
+
+}
