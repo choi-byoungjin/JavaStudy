1971499c539bbc1f2628f17cb12f76bf4db9999e
choibyoungjin <choibyoungjin85@gmail.com>
2021-05-08 23:56:08 +0900

    showStudentInfo 메서드 추가

--- Chapter5/src/Chapter5/Student.java
+++ Chapter5/src/Chapter5/Student.java
@@ -5,4 +5,8 @@ public class Student {//클래스 이름은 첫글자 대문자로 한다
 	String studentName;	//학생 이름
 	int grade;			//학년
 	String address;		//사는 곳 : 멤버 변수(속성)
+	
+	public void showStudentInfo() {
+		System.out.println(studentName + "," + address);
+	}
 }
