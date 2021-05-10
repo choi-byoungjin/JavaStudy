7b5b6790035ef1ab595140959184304b3c2b1b53
choibyoungjin <choibyoungjin85@gmail.com>
2021-05-11 08:33:43 +0900

    테스트 클래스 구현하기

--- Chapter5/src/Chapter5_5/Person.java
+++ Chapter5/src/Chapter5_5/Person.java
@@ -5,8 +5,15 @@ public class Person {
 	float height;
 	float weight;
 	
+	public Person() {}//자바 컴파일러가 자동으로 제공하는 디폴트 생성자
+
 	public Person(String pname) {
 		name = pname;//이름을 매개변수로 입력받아서 Person 클래스를 생성하는 생성자
 	}
-	public Person() {}//자바 컴파일러가 자동으로 제공하는 디폴트 생성자
+	
+	public Person(String pname, float pheight, float pweight) {//이름, 키, 몸무게를 매개변수로 입력받는 생성자
+		name = pname;
+		height = pheight;
+		weight = pweight;
+	}
 }
