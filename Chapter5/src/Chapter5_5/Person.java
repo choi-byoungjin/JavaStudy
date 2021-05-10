faca8fa3bb3a7021de32e6db302c230ba01f64b3
choibyoungjin <choibyoungjin85@gmail.com>
2021-05-11 07:44:25 +0900

    디폴트 생성자

--- Chapter5/src/Chapter5_5/Person.java
+++ Chapter5/src/Chapter5_5/Person.java
@@ -4,4 +4,6 @@ public class Person {
 	String name;
 	float height;
 	float weight;
+	
+	public Person() {}//자바 컴파일러가 자동으로 제공하는 디폴트 생성자
 }
---
+++ Chapter5/src/Chapter5_5/PersonTest.java
@@ -0,0 +1,10 @@
+package Chapter5_5;
+
+public class PersonTest {
+
+	public static void main(String[] args) {
+		Person personLee = new Person();
+
+	}
+
+}
