a3dd077f851ca592de1e5dbe0120aa81f7b89c35
choibyoungjin <choibyoungjin85@gmail.com>
2021-05-11 09:54:42 +0900

    private 변수에 접근하기

--- Chapter5/src/Chapter5/StudentTest.java
+++ Chapter5/src/Chapter5/StudentTest.java
@@ -4,9 +4,9 @@ public class StudentTest {
 
 	public static void main(String[] args) {
 		Student studentAhn = new Student();
-		studentAhn.studentName = "¾È½Â¿¬";
-		
-		System.out.println(studentAhn.studentName);
+		//studentAhn.studentName = "¾È½Â¿¬";
+		studentAhn.setStudentName("¾È½Â¿¬");
+		//System.out.println(studentAhn.studentName);
 		System.out.println(studentAhn.getStudentName());
 	}
 
