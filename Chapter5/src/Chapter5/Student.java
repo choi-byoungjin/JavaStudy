68ab686654912785921651d76e6581ad93780d13
choibyoungjin <choibyoungjin85@gmail.com>
2021-05-11 09:21:17 +0900

    private 사용하기

--- Chapter5/src/Chapter5/Student.java
+++ Chapter5/src/Chapter5/Student.java
@@ -2,7 +2,7 @@ package Chapter5;
 
 public class Student {//클래스 이름은 첫글자 대문자로 한다
 	int studentID;		//학번
-	String studentName;	//학생 이름
+	private String studentName;	//학생 이름
 	int grade;			//학년
 	String address;		//사는 곳 : 멤버 변수(속성)
 	
