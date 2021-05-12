48887fb87cb98934e62194ea2e21b4a6d8f3cff4
choibyoungjin <choibyoungjin85@gmail.com>
2021-05-12 16:46:56 +0900

    this를 사용하여 주소 값 변환하기

--- Chapter6/src/chapter6/CallAnotherConst.java
+++ Chapter6/src/chapter6/CallAnotherConst.java
@@ -1,23 +1,32 @@
 package chapter6;
+
+class Person{
+	String name;
+	int age;
 	
-	class Person{
-		String name;
-		int age;
-		
-		Person(){
-			this("이름 없음", 1);
-		}
-		
-		Person(String name, int age){
-			this.name = name;
-			this.age = age;
-		}
+	Person(){
+		this("이름 없음", 1);//Person(String, int) 생성자 호출
 	}
-	public class CallAnotherConst {
-		public static void main(String[] args) {
-			Person noName = new Person();
-			System.out.println(noName.name);
-			System.out.println(noName.age);
+	
+	Person(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
+	
+	Person returnItSelf() {//반환형은 클래스형
+		return this;//this 반환
+	}
+}
+public class CallAnotherConst {
+	public static void main(String[] args) {
+		Person noName = new Person();
+		System.out.println(noName.name);
+		System.out.println(noName.age);
+		
+		Person p = noName.returnItSelf(); //this값을 클래스 변수에 대입
+		System.out.println(p);			//noName.returnItSelf()의 반환 값 출력
+		System.out.println(noName);		//참조 변수 출력
+
 	}
 
 }
