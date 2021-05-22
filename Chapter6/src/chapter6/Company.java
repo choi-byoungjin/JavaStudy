23eade3bbaf574ac3e27885c8cf7d9c428976521
choibyoungjin <choibyoungjin85@gmail.com>
2021-05-23 04:17:42 +0900

    public 메서드 수정

--- Chapter6/src/chapter6/Company.java
+++ Chapter6/src/chapter6/Company.java
@@ -9,6 +9,6 @@ public class Company {
 		if(instance == null) {
 			instance = new Company();
 		}
-		return instance;
-	}
+		return instance;// 유일하게 생성한 인스턴스 반환
+	}// 인스턴스를 외부에서 참조할 수 있도록 public get() 메서드 구현
 }
