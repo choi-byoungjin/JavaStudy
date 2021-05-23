4c71efc64e3523f631bb4f1e266ade019b48c1d6
choibyoungjin <choibyoungjin85@gmail.com>
2021-05-24 08:39:46 +0900

    객체 배열 만들기(2)

--- Chapter7/src/chapter7/Book.java
+++ Chapter7/src/chapter7/Book.java
@@ -9,7 +9,7 @@ public class Book {
 	public Book(String bookName, String author) {
 		this.bookName = bookName;
 		this.author = author;
-	}
+	}//책 이름과 저자 이름을 매개변수로 받는 생성자
 	public String getBookName() {
 		return bookName;
 	}
@@ -24,5 +24,5 @@ public class Book {
 	}
 	public void showBookInfo() {
 		System.out.println(bookName + "," + author);
-	}
+	}//책 정보를 출력해 주는 메서드
 }
