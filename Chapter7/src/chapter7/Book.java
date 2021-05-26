39b461959ffbcfb9a34a470222c09ae556a34940
choibyoungjin <choibyoungjin85@gmail.com>
2021-05-26 23:59:49 +0900

    객체 배열의 깊은 복사

--- Chapter7/src/chapter7/Book.java
+++ Chapter7/src/chapter7/Book.java
@@ -10,18 +10,23 @@ public class Book {
 		this.bookName = bookName;
 		this.author = author;
 	}//책 이름과 저자 이름을 매개변수로 받는 생성자
+	
 	public String getBookName() {
 		return bookName;
 	}
+			
 	public void setBookName(String bookName) {
 		this.bookName = bookName;
 	}
+	
 	public String getAuthor() {
 		return author;
 	}
+	
 	public void setAuthor(String author) {
 		this.author = author;
 	}
+	
 	public void showBookInfo() {
 		System.out.println(bookName + "," + author);
 	}//책 정보를 출력해 주는 메서드
