fa590448e038d1c1a9e8445255ef780c3418d34c
choibyoungjin <choibyoungjin85@gmail.com>
2021-05-27 19:52:38 +0900

    Subject 클래스 구현하기

---
+++ Chapter7/src/chapter7/Subject.java
@@ -0,0 +1,23 @@
+package chapter7;
+
+public class Subject {
+	private String name;	//과목 이름
+	private int scorePoint;	//과목 점수
+	//Subject 클래스의 멤버 변수
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public int getScorePoint() {
+		return scorePoint;
+	}
+	
+	public void setScorePiont(int scorePiont) {
+		this.scorePoint = scorePiont;
+	}
+}
