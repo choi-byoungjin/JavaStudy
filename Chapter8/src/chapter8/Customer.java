ebfe63edeaf725923b7b9ea7c02cff160e4e32ac
choibyoungjin <choibyoungjin85@gmail.com>
2021-05-28 16:17:42 +0900

    protected 변수 선언

--- Chapter8/src/chapter8/Customer.java
+++ Chapter8/src/chapter8/Customer.java
@@ -1,9 +1,9 @@
 package chapter8;
 
 public class Customer {
-	private int customerID;			//고객 아이디
-	private String customerName;	//고객 이름
-	private String customerGrade;	//고객 등급
+	protected int customerID;			//고객 아이디
+	protected String customerName;	//고객 이름
+	protected String customerGrade;	//고객 등급
 	int bonusPoint;					//보너스 포인트
 	double bonusRatio;				//적립 비율
 	//멤버 변수
@@ -12,6 +12,30 @@ public class Customer {
 		bonusRatio = 0.01;			//보너스 포인트 기본 적립 비율
 	}//디폴트 생성자
 	
+	public int getCustomerID() {
+		return customerID;
+	}
+
+	public void setCustomerID(int customerID) {
+		this.customerID = customerID;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getCustomerGrade() {
+		return customerGrade;
+	}
+
+	public void setCustomerGrade(String customerGrade) {
+		this.customerGrade = customerGrade;
+	}//protected 예약어로 선언한 변수를 외부에서 사용할 수 있도록 get(), set() 메서드 추가
+
 	public int calcPrice(int price) {
 		bonusPoint += price * bonusRatio;//보너스 포인트 계산
 		return price;
