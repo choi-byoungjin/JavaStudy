06d92de130d49d3b48ab2816d71431c0a2ac39e1
choibyoungjin <choibyoungjin85@gmail.com>
2021-06-01 01:14:45 +0900

    명시적으로 상위 클래스 생성자 호출하기

--- Chapter8/src/chapter8/VIPCustomer.java
+++ Chapter8/src/chapter8/VIPCustomer.java
@@ -5,6 +5,7 @@ public class VIPCustomer extends Customer {//VIPCustomer 클래스는 Customer 클래�
 	double saleRatio;		//할인율
 	
 	public VIPCustomer() {
+		super(customerID, customerName);//상위 클래스 생성자 호출
 		customerGrade = "VIP";//상위 클래스에서 private 변수이므로 오류 발생
 		bonusRatio = 0.05;
 		saleRatio = 0.1;
