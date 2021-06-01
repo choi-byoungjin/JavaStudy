dbe97bafadfb1ab0bd2ccc88b77a05b2b73fa2d8
choibyoungjin <choibyoungjin85@gmail.com>
2021-06-02 08:44:35 +0900

    고객 관리 프로그램 완성하기(1)

--- Chapter8/src/chapter8/Customer.java
+++ Chapter8/src/chapter8/Customer.java
@@ -8,18 +8,19 @@ public class Customer {
 	double bonusRatio;				//적립 비율
 	//멤버 변수
 	public Customer() {
-		customerGrade = "SILVER";	//기본 등급
-		bonusRatio = 0.01;			//보너스 포인트 기본 적립 비율
-		//System.out.println("Customer( ) 생성자 호출");//상위 클래스 생성할 때 콘솔 출력문
+		initCustomer()
 	}//디폴트 생성자
 	
 	public Customer(int customerID, String customerName) {
 		this.customerID = customerID;
 		this.customerName = customerName;
+		initCustomer()//고객 등급과 보너스 포인트 적립률 지정 함수 호출
+	}
+	
+	private void initCustomer() {//생성자에서만 호출하는 메서드이므로 private으로 선언
 		customerGrade = "SILVER";
 		bonusRatio = 0.01;
-		//System.out.println("Customer(int, String) 생성자 호출");
-	}
+	}//멤버 변수의 초기화 부분
 	
 	public int getCustomerID() {
 		return customerID;
