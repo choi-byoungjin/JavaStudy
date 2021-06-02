785548467ad1a8da02d96bfb18166b36e825ce94
choibyoungjin <choibyoungjin85@gmail.com>
2021-06-02 12:46:08 +0900

    추상 메서드 구현하기

--- Chapter9/src/chapter9/DeskTop.java
+++ Chapter9/src/chapter9/DeskTop.java
@@ -2,4 +2,16 @@ package chapter9;
 
 public class DeskTop extends Computer{
 
+	@Override
+	public void display() {
+		System.out.println("DeskTop display()");
+		
+	}
+
+	@Override
+	public void typing() {
+		System.out.println("DeskTop typing()");
+		
+	}
+
 }
