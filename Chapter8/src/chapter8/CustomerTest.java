2bc0f0b4e946a2d9862e68c0165e8fe187dc3be2
choibyoungjin <choibyoungjin85@gmail.com>
2021-06-02 10:02:12 +0900

    배열을 활용한 고객 관리 프로그램 구현하기

--- Chapter8/src/chapter8/CustomerTest.java
+++ Chapter8/src/chapter8/CustomerTest.java
@@ -1,28 +1,35 @@
 package chapter8;
+import java.util.ArrayList;
 
 public class CustomerTest {
 	public static void main(String[] args) {
-		Customer customerLee = new Customer();
-		customerLee.setCustomerID(10010);
-		customerLee.setCustomerName("이순신");
-		customerLee.bonusPoint = 1000;
+		ArrayList<Customer> customerList = new ArrayList<Customer>();
 		
-		System.out.println(customerLee.showCustomerInfo());
+		Customer customerLee = new Customer(10010, "이순신");
+		Customer customerShin = new Customer(10020, "신사임당");
+		Customer customerHong = new Customer(10030, "홍길동");
+		Customer customerYoul = new Customer(10040, "이율곡");
+		Customer customerKim = new Customer(10050, "김유신", 12345);
 		
-		Customer customerKim = new VIPCustomer(10020, "김유신", 12345);//VIPCustomer를 Customer형으로 선언
-		customerKim.bonusPoint = 1000;
+		customerList.add(customerLee);
+		customerList.add(customerKim);
+		customerList.add(customerYoul);
+		customerList.add(customerHong);
+		customerList.add(customerShin);
 		
-		System.out.println(customerKim.showCustomerInfo());
+		System.out.println("====== 고객 정보 출력 ======");
+		for(Customer customer : customerList) {
+			System.out.println(customer.showCustomerInfo());
+		}
+	
 		System.out.println("====== 할인율과 보너스 포인트 계산 ======");
 		
 		int price = 10000;
-		int leePrice = customerLee.calcPrice(price);
-		int kimPrice = customerKim.calcPrice(price);
-		
-		System.out.println(customerLee.getCustomerName() + " 님이 " + leePrice + "원 지불하셨습니다.");
-		System.out.println(customerLee.showCustomerInfo());
-		System.out.println(customerKim.getCustomerName() + " 님이 " + kimPrice + "원 지불하셨습니다.");
-		System.out.println(customerKim.showCustomerInfo());
+		for(Customer customer : customerList) {
+			int cost = customer.calcPrice(price);
+			System.out.println(customer.getCustomerName() + " 님이 " + cost + "원 지불하셨습니다.");
+			System.out.println(customer.getCustomerName() + " 님의 현재 보너스 포인트는 " + customer.bonusPoint + "점입니다.");
+		}
 	}
 
 }
