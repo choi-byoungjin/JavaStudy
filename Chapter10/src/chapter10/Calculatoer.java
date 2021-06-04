0e24c9030e64bb31855ce65ec9e55670eafa3f8c
choibyoungjin85 <choibyoungjin85@gmail.com>
2021-06-05 01:07:19 +0900

    인터페이스 구현하기

---
+++ Chapter10/src/chapter10/Calculatoer.java
@@ -0,0 +1,17 @@
+package chapter10;
+
+public abstract class Calculatoer implements Calc {
+
+	@Override
+	public int add(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return num1 + num2;
+	}
+
+	@Override
+	public int divide(int num1, int num2) {
+		// TODO Auto-generated method stub
+		return num1 - num2;
+	}
+
+}
