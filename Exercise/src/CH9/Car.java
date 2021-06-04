a1c0e0e8da69a9e3374aa794c833801f0e183ec2
choibyoungjin85 <choibyoungjin85@gmail.com>
2021-06-04 22:46:02 +0900

    Q6

--- Exercise/src/CH9/Car.java
+++ Exercise/src/CH9/Car.java
@@ -6,10 +6,14 @@ public abstract class Car {
 	public abstract void startCar();
 	public abstract void turnOff();
 	
+	public void wash() {
+		System.out.println("세차를 합니다.");
+	}
 	final public void run() {
 		startCar();
 		drive();
 		stop();
 		turnOff();
+		wash();
 	}//템플릿 메서드
 }
