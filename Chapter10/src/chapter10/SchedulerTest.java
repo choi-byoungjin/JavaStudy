f138771ed8130ade109364db447a396a51dbd37f
choibyoungjin <choibyoungjin85@gmail.com>
2021-06-12 15:12:34 +0900

    나 혼자 코딩 326p

--- Chapter10/src/chapter10/SchedulerTest.java
+++ Chapter10/src/chapter10/SchedulerTest.java
@@ -9,6 +9,7 @@ public class SchedulerTest {
 		System.out.println("R : 한명씩 차례로 할당 ");
 		System.out.println("L : 쉬고 있거나 대기가 가장 적은 상담원에게 할당 ");
 		System.out.println("P : 우선순위가 높은 고객 먼저 할당 ");
+		System.out.println("A : 상담원이 전화 가져감");
 		
 		int ch = System.in.read();//할당 방식을 입력받아 ch 변수에 대입
 		Scheduler scheduler = null;
@@ -22,6 +23,9 @@ public class SchedulerTest {
 		else if(ch == 'P'|| ch == 'p'){
 			scheduler = new PriorityAllocation();
 		}//입력받은 값이 P 또는 p 이면 PriorityAllocation 클래스 생성
+		else if(ch == 'A'|| ch == 'a') {
+			scheduler = new AgentGetCall();
+		}
 		else{
 			System.out.println("지원되지 않는 기능입니다.");
 			return;
