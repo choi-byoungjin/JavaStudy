7a7bc0f3da596b08d9f2350704ee9c4b1b8f864a
choibyoungjin85 <choibyoungjin85@gmail.com>
2021-06-15 21:12:47 +0900

    Calc 인터페이스에 private 메서드 구현하기

--- Chapter10/src/chapter10/Calc.java
+++ Chapter10/src/chapter10/Calc.java
@@ -11,6 +11,7 @@ public interface Calc {
 	
 	default void description() {
 		System.out.println("정수 계산기를 구현합니다.");
+		myMethod();
 	}
 	
 	static int total(int[] arr) {
@@ -19,6 +20,15 @@ public interface Calc {
 		for(int i : arr) {
 			total += i;
 		}
+		myStaticMethod();
 		return total;
+	}
+	
+	private void myMethod() {
+		System.out.println("private 메서드입니다.");
+	}
+	
+	private static void myStaticMethod() {
+		System.out.println("private static 메서드입니다.");
 	}
 }
