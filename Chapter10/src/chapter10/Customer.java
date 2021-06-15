3943bf9642dd56504b32082f06d311c6ed3d1355
choibyoungjin85 <choibyoungjin85@gmail.com>
2021-06-16 00:55:40 +0900

    디폴트 메서드 중복, Customer클래스에서 재정의

--- Chapter10/src/chapter10/Customer.java
+++ Chapter10/src/chapter10/Customer.java
@@ -14,4 +14,9 @@ public class Customer implements Buy, Sell{//Customer 클래스는 Buy와 Sell인터페�
 		
 	}
 
+	@Override
+	public void order() {
+		System.out.println("고객 판매 주문");//디폴트 메서드 order()를 Customer 클래스에서 재정의함
+	}
+
 }
