499603b95df8996a9cc13eb57b7fb532e8baf74d
choibyoungjin85 <choibyoungjin85@gmail.com>
2021-06-15 19:49:21 +0900

    정적 메서드 구현하기

--- Chapter10/src/chapter10/Calc.java
+++ Chapter10/src/chapter10/Calc.java
@@ -12,4 +12,13 @@ public interface Calc {
 	default void description() {
 		System.out.println("정수 계산기를 구현합니다.");
 	}
+	
+	static int total(int[] arr) {
+		int total = 0;
+		
+		for(int i : arr) {
+			total += i;
+		}
+		return total;
+	}
 }
