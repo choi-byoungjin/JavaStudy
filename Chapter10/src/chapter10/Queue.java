8b4490b1c81669e0aca18d007e85f0d2651704e2
choibyoungjin85 <choibyoungjin85@gmail.com>
2021-06-16 01:36:11 +0900

    Queue 주석

--- Chapter10/src/chapter10/Queue.java
+++ Chapter10/src/chapter10/Queue.java
@@ -6,3 +6,5 @@ public interface Queue {
 	String deQueue();			//배열의 맨 처음 항목 반환
 	int getSize();				//현재 Queue에 있는 개수 반환
 }
+//enQueue()는 입력되는 요소 값을 배열의 맨 뒤에 추가합니다.
+//deQueue()는 배열에서 맨 앞에 있는 요소를 제거하고 그 값을 반환합니다.
