4b69e463640723f8d0608e9c3ba140446e2ad55e
choibyoungjin85 <choibyoungjin85@gmail.com>
2021-06-17 01:27:29 +0900

    toString() 메서드 재정의하기

---
+++ Chapter11/src/Alone/StudentTest.java
@@ -0,0 +1,10 @@
+package Alone;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		Student studentHong = new Student(1001, "ȫ�浿");
+		System.out.println(studentHong);
+	}
+
+}
