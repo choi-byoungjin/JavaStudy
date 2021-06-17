bc89db6335a841e167a0e341f1e3f2d5eb718098
choibyoungjin85 <choibyoungjin85@gmail.com>
2021-06-18 01:25:26 +0900

    GenericPrinter<T> 클래스 정의하기

--- Chapter12/src/chapter12/GenericPrinter.java
+++ Chapter12/src/chapter12/GenericPrinter.java
@@ -1,7 +1,7 @@
 package chapter12;
 
 public class GenericPrinter<T> {//제네릭 클래스 / Type의 약자. 자료형 매개변수
-	private T material;
+	private T material;//T 자료형으로 선언한 변수
 	
 	public void setMaterial(T material) {
 		this.material = material;
@@ -9,5 +9,9 @@ public class GenericPrinter<T> {//제네릭 클래스 / Type의 약자. 자료형 매개변수
 	
 	public T getMaterial() {
 		return material;
+	}//T 자료형 변수 material을 반환하는 제네릭 메서드
+	
+	public String toString() {
+		return material.toString();
 	}
 }
