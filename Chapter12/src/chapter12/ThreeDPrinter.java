f2bb3366d0a76bb602be484c045a6ee0b770a5d7
choibyoungjin85 <choibyoungjin85@gmail.com>
2021-06-18 01:18:16 +0900

    제네릭

---
+++ Chapter12/src/chapter12/ThreeDPrinter.java
@@ -0,0 +1,13 @@
+package chapter12;
+
+public class ThreeDPrinter {
+	private Object material;//재료가 파우더인 경우
+	
+	public void setMaterial(Object material) {
+		this.material = material;
+	}
+	
+	public Object getMaterial() {
+		return material;
+	}
+}
